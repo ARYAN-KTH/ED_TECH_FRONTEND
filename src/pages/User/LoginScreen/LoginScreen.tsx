5110d5754bd47150996a5ee1267e8bd0f1013fe3
KTH-ARYAN <aryan@kriscent.in>
2025-03-23 22:01:08 +0530

    fhfg

--- src/pages/User/LoginScreen/LoginScreen.tsx
+++ src/pages/User/LoginScreen/LoginScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
--- src/pages/User/SignUpFlow/SignUpScreen.tsx
+++ src/pages/User/SignUpFlow/SignUpScreen.tsx
@@ -30,6 +30,7 @@ const SignUpScreen = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [activeTab, setActiveTab] = useState("Student");
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const googleSignupHandler = async () => {
     try {
       const result = await signInWithGoogle();
